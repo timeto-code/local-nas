b815326e9717009770012ad90a19d35694e642aa
timeto-code <niki.w.yg@outlook.com>
2024-11-18 13:05:50 +0800

    搜索输入框添加防抖功能

--- components/main/SearchBar.tsx
+++ components/main/SearchBar.tsx
@@ -3,9 +3,19 @@
 import { GoSearch } from 'react-icons/go';
 
 import { useSearchFileStore } from '@/store';
+import { useRef } from 'react';
 import Category from './Category';
 
 const SearchBar = () => {
+  const debounceTimerId = useRef<ReturnType<typeof setTimeout> | null>(null);
+
+  const handleOnchange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (debounceTimerId.current) clearTimeout(debounceTimerId.current);
+    debounceTimerId.current = setTimeout(() => {
+      useSearchFileStore.getState().setSearch(e.target.value);
+    }, 500);
+  };
+
   return (
     <div className="flex h-full flex-col items-center justify-center">
       <div className="relative flex w-[80%] max-w-[650px] items-center justify-center">
@@ -14,9 +24,7 @@ const SearchBar = () => {
           type="text"
           placeholder="搜索文件"
           className="h-12 w-full rounded-full bg-hover py-2 pl-12 pr-4 placeholder:text-foreground focus:bg-background focus:shadow-bottom-1px focus:outline-none"
-          onChange={(e) => {
-            useSearchFileStore.getState().setSearch(e.target.value);
-          }}
+          onChange={handleOnchange}
         />
       </div>
       <Category />
